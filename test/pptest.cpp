d99e3c624cc4638a0313f133b87d2856d09552bd
jjonsson <jay_jonsson@hmc.edu>
2013-04-12 21:24:28 -0700

    expanded test file

--- test/pptest.cpp
+++ test/pptest.cpp
@@ -9,11 +9,21 @@ using namespace pp;
 
 int main()
 {
-  unordered_map<int, string> mymap{make_pair(42, "foo"), make_pair(5, "bar")};
   print("hello", "world", 42, 3.14);
+
   print(make_tuple("tuples", "inside", make_tuple("tuples")));
+
   print();
+
+  unordered_map<int, string> mymap{make_pair(42, "foo"), make_pair(5, "bar")};
   print(mymap, "weeeeeeeeeeeee");
+
   int myarray[3] = {1, 2, 3};
   print(myarray);
+
+  // only the address will be printed
+  int* myheaparray = new int[5];
+  myheaparray[0] = 1;
+  myheaparray[4] = 5;
+  print(myheaparray);
 }
