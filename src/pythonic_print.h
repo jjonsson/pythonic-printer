564ae66625efd64c99a0087a1200211507458e42
James Jonsson <jay_jonsson@hmc.edu>
2013-04-13 00:12:44 -0700

    #included ostream

--- src/pythonic_print.h
+++ src/pythonic_print.h
@@ -4,6 +4,8 @@
 #ifndef PYTHONIC_PRINT_H
 #define PYTHONIC_PRINT_H
 
+#include <ostream>
+
 namespace pp {
   // This print function prints in a style similar to Python's print.
   // Pass in whatever you want!
@@ -13,7 +15,7 @@ namespace pp {
   // To print to a stream other than std::cout, pass it in as the first
   //   argument. If C++ had named arguments it would be more pythonic.
   template<typename... Ts>
-  void print(ostream&, const Ts&...);
+  void print(std::ostream&, const Ts&...);
 }
 
 #include "pythonic_print-private.h"
