db26eef9249c3f07ed048d08dad33b954b8816f4
jjonsson <jay_jonsson@hmc.edu>
2013-04-12 20:41:09 -0700

    removed extraneous std prefixes

--- src/pythonic_print-private.h
+++ src/pythonic_print-private.h
@@ -6,13 +6,14 @@
 #include <utility>
 #include <tuple>
 
+using namespace std;
+
 namespace pp
 {
-
   // uses SFINAE to determine if the type supports iterators
   template<typename T>
   static constexpr auto has_begin_and_end(int x) ->
-    decltype(std::declval<T>().begin(), std::declval<T>().end(), bool())
+    decltype(declval<T>().begin(), declval<T>().end(), bool())
   {
     return true;
   }
@@ -28,7 +29,7 @@ namespace pp
   // if the type has the stream insert operator, <<, we can use it to print
   template<typename T>
   static constexpr auto has_stream_insert(int x) ->
-    decltype(std::cout << std::declval<T>(), bool())
+    decltype(cout << declval<T>(), bool())
   {
     return true;
   }
@@ -57,53 +58,75 @@ namespace pp
 
   // specialization for pairs
   template<typename T1, typename T2>
-  class PrintHelper<std::pair<T1, T2>>
+  class PrintHelper<pair<T1, T2>>
   {
   public:
-    static void print(std::pair<T1, T2> t);
+    static void print(pair<T1, T2> t);
   };
 
   // specialization for tuples
   template<typename... Ts>
-  class PrintHelper<std::tuple<Ts...>>
+  class PrintHelper<tuple<Ts...>>
   {
   public:
-    static void print(std::tuple<Ts...> t);
+    static void print(tuple<Ts...> t);
   };
 
   // if operator<<(ostream, T) is defined, just use that
   template<typename T>
   static auto print_helper(T first) ->
-    typename std::enable_if<has_stream_insert<T>(0)>::type
+    typename enable_if<has_stream_insert<T>(0)
+                    && (!is_array<T>::value)>::type
   {
-    std::cout << first;
+    cout << first;
+  }
+
+  // T is an array
+  template<typename T>
+  static auto print_helper(T object) ->
+    typename enable_if<is_array<T>::value>::type
+  {
+    cerr << "got to array printer" << endl;
+    // just in case a null pointer gets passed in, though I doubt a null pointer
+    //   can ever be passed into this function because of the enable_if
+    if (object == NULL) cout << "{}";
+
+    int length = sizeof(object) / sizeof(object[0]);
+
+    cout << "{";
+    PrintHelper<typename T::value_type>::print(object[0]);
+    for (int i = 1; i < length; ++i) {
+      cout << ", ";
+      PrintHelper<typename T::value_type>::print(object[i]);
+    }
+    cout << "}";
   }
 
   // fallback case for when there is not a simple way to print the given type
   template<typename T>
   static auto print_helper(T first) ->
-    typename std::enable_if<(not has_stream_insert<T>(0))
-                        and (not has_begin_and_end<T>(0))>::type
+    typename enable_if<(!has_stream_insert<T>(0))
+                    && (!has_begin_and_end<T>(0))>::type
   {
-    std::cout << "[[X]]";
+    cout << "[[X]]";
   }
 
   // if the given type has iterators, print it using initializer list syntax
   template<typename T>
   static auto print_helper(T first) ->
-    typename std::enable_if<(has_begin_and_end<T>(0))
-                        and (not has_stream_insert<T>(0))>::type
+    typename enable_if<(has_begin_and_end<T>(0))
+                    && (!has_stream_insert<T>(0))>::type
   {
     auto it = first.begin();
-    std::cout << "{";
+    cout << "{";
     if (it != first.end()) {
       PrintHelper<typename T::value_type>::print(*it);
     }
     for (++it; it != first.end(); ++it) {
-      std::cout << ", ";
+      cout << ", ";
       PrintHelper<typename T::value_type>::print(*it);
     }
-    std::cout << "}";
+    cout << "}";
   }
 
   // when called on a type that is not a pair or tuple, we can let print_helper
@@ -114,49 +137,48 @@ namespace pp
     print_helper<T>(t);
   }
 
-  // base case when the index of the element we are printing is outside the range
-  //   of printable elements
+  // base case when the index of the element we are printing is outside the
+  //   range of printable elements
   template<typename T, int I>
   static auto print_tuple(T t) ->
-    typename std::enable_if<I >= std::tuple_size<T>::value>::type {}
+    typename enable_if<I >= tuple_size<T>::value>::type {}
 
   // prints element I of a tuple of type T
   // element 0 should be printed with PrintHelper::print before this function
   //   is called on element 1
   template<typename T, int I>
   static auto print_tuple(T t) ->
-    typename std::enable_if<I < std::tuple_size<T>::value>::type
+    typename enable_if<I < tuple_size<T>::value>::type
   {
-    std::cout << ", ";
-    PrintHelper<typename std::tuple_element<I, T>::type>::print(std::get<I>(t));
+    cout << ", ";
+    PrintHelper<typename tuple_element<I, T>::type>::print(get<I>(t));
     print_tuple<T, I + 1>(t);
   }
 
   // printing a pair is pretty easy
   template<typename T1, typename T2>
-  void PrintHelper<std::pair<T1, T2>>::print(std::pair<T1, T2> t)
+  void PrintHelper<pair<T1, T2>>::print(pair<T1, T2> t)
   {
-    std::cout << "(";
-    PrintHelper<T1>::print(std::get<0>(t));
-    std::cout << ", ";
-    PrintHelper<T2>::print(std::get<1>(t));
-    std::cout << ")";
-
+    cout << "(";
+    PrintHelper<T1>::print(get<0>(t));
+    cout << ", ";
+    PrintHelper<T2>::print(get<1>(t));
+    cout << ")";
   }
 
   // print element 0 then call print_tuple on the rest of the tuple
   // this is to print the correct number of commas
   template<typename... Ts>
-  void PrintHelper<std::tuple<Ts...>>::print(std::tuple<Ts...> t)
+  void PrintHelper<tuple<Ts...>>::print(tuple<Ts...> t)
   {
-    std::cout << "(";
+    cout << "(";
     // if the tuple is empty, just print parens
-    if (std::tuple_size<std::tuple<Ts...>>::value > 0) {
-      PrintHelper<typename std::tuple_element<0, std::tuple<Ts...>>::type>::print(std::get<0>(t));
+    if (tuple_size<tuple<Ts...>>::value > 0) {
+      PrintHelper<typename tuple_element<0, tuple<Ts...>>::type>::
+        print(get<0>(t));
     }
-    print_tuple<std::tuple<Ts...>, 1>(t);
-    std::cout << ")";
-
+    print_tuple<tuple<Ts...>, 1>(t);
+    cout << ")";
   }
 
   // base case
@@ -164,7 +186,7 @@ namespace pp
   void print(T first)
   {
     PrintHelper<T>::print(first);
-    std::cout << std::endl;
+    cout << endl;
   }
 
   // accepts any number/combination of arguments and types of arguments
@@ -172,7 +194,7 @@ namespace pp
   void print(T first, Ts... rest)
   {
     PrintHelper<T>::print(first);
-    std::cout << " ";
+    cout << " ";
     print<Ts...>(rest...);
   }
 
